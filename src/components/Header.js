52fd3042b0c330618e9a3f346a15fc5266383340
mickeymaruf <thegoldenpopo@gmail.com>
2022-10-20 23:39:33 +0600

    fixed header toggle minimize on click

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,23 +8,26 @@ const Header = () => {
     const { user, logOut } = useAuth();
     const [toggle, setToggle] = useState(false);
     const navigate = useNavigate();
+    const minimizeToggle = () => {
+        setToggle(false);
+    }
     return (
         <header className='bg-slate-900 py-5'>
             <nav className='w-11/12 mx-auto flex items-center gap-6 relative'>
                 <Link to="/"><img className='w-10' src={logo} alt="logo" /></Link>
-                <div className={`text-white text-lg md:text-base font-medium flex gap-5 md:gap-8 flex-col md:flex-row z-50 bg-slate-700 backdrop-blur-md bg-opacity-70 md:bg-inherit fixed md:static right-0 ${toggle ? 'top-20' : '-top-[350px]'} w-full items-center justify-center px-16 py-5 md:p-0 rounded-lg md:ml-2 duration-300`}>
-                    <NavLink to="/pricing" className='hover:text-indigo-400'>Pricing</NavLink>
-                    <NavLink className='hover:text-indigo-400'>Company</NavLink>
-                    <NavLink to="/resources" className='hover:text-indigo-400'>Resources</NavLink>
+                <div className={`text-white text-lg md:text-base font-medium flex gap-5 md:gap-8 flex-col md:flex-row z-50 bg-slate-700 backdrop-blur-md bg-opacity-70 md:bg-inherit fixed md:static right-0 ${toggle ? 'top-20' : '-top-[350px]'} w-full items-center justify-center px-16 py-8 md:p-0 rounded-lg md:ml-2 duration-300`}>
+                    <NavLink onClick={minimizeToggle} to="/pricing" className='hover:text-indigo-400'>Pricing</NavLink>
+                    <NavLink onClick={minimizeToggle} className='hover:text-indigo-400'>Company</NavLink>
+                    <NavLink onClick={minimizeToggle} to="/resources" className='hover:text-indigo-400'>Resources</NavLink>
                     {
                         user && user.uid ?
                             <div className='ml-auto'>
-                                <NavLink to="/profile" className='hover:text-indigo-400 mr-5'>Profile</NavLink>
-                                <NavLink to="/wallet" className='hover:text-indigo-400 mr-5'>Wallet</NavLink>
-                                <button onClick={logOut} className='ml-0 md:ml-auto bg-indigo-500 py-3 px-6 rounded-lg text-white font-medium hover:shadow-lg hover:shadow-slate-700'>Logout</button>
+                                <NavLink onClick={minimizeToggle} to="/profile" className='hover:text-indigo-400 mr-5'>Profile</NavLink>
+                                <NavLink onClick={minimizeToggle} to="/wallet" className='hover:text-indigo-400 mr-5'>Wallet</NavLink>
+                                <button onClick={() => { logOut(); setToggle(false) }} className='ml-0 md:ml-auto bg-indigo-500 py-3 px-6 rounded-lg text-white font-medium hover:shadow-lg hover:shadow-slate-700'>Logout</button>
                             </div>
                             :
-                            <button onClick={() => navigate('/login')} className='ml-0 md:ml-auto bg-indigo-500 py-3 px-6 rounded-lg text-white font-medium hover:shadow-lg hover:shadow-slate-700'>Login</button>
+                            <button onClick={() => { navigate('/login'); setToggle(false) }} className='ml-0 md:ml-auto bg-indigo-500 py-3 px-6 rounded-lg text-white font-medium hover:shadow-lg hover:shadow-slate-700'>Login</button>
                     }
                 </div>
                 <div className='ml-auto'>
